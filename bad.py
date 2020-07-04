63ab1d3f9d3c106ada1fb4147c15f6315c53e714
Sebastian Rüth <sebirueth@gmx.de>
2020-07-04 18:45:15 +0200

    initial commit

---
+++ bad.py
@@ -0,0 +1,36 @@
+import math
+
+
+def main():
+    f = open("numbers.txt", "r")
+    i = 0
+    lines = f.read().split(',')
+    for i in range(0, len(lines)):
+        lines[i] = int(lines[i])
+    n = len(lines)
+
+    qsort(lines, 0, n - 1)
+    for i in range(n):
+        print("%s" %lines[i])
+    
+        
+def qsort(arr, low, high):
+    if(low < high):
+        pi = partition(arr, low, high)
+        qsort(arr, low, pi - 1)
+        qsort(arr, pi + 1, high)
+    
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = (low - 1)
+    j = low
+    while (j <= high - 1):
+        if arr[j] < pivot:
+            i = i + 1
+            arr[i],arr[j] = arr[j],arr[i]
+        j = j + 1
+    arr[i+1],arr[high] = arr[high],arr[i+1]
+    return (i + 1)
+
+
+main()
